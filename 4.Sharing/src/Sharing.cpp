ebb5e1e8ca1fd59952829e7d97d68f3d337aebda
zwt153841949 <49278117+zwt153841949@users.noreply.github.com>
2019-04-18 19:24:48 +0800

    Update Sharing.cpp

--- 4.Sharing/src/Sharing.cpp
+++ 4.Sharing/src/Sharing.cpp
@@ -5,41 +5,10 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-//Output Specification:
 
-//For each case, simply output the 5-digit starting position of the common suffix.
-//If the two words have no common suffix, output -1 instead.
-//
-//Sample Input 1:
-//
-//11111 22222 9
-//67890 i 00002
-//00010 a 12345
-//00003 g -1
-//12345 D 67890
-//00002 n 00003
-//22222 B 23456
-//11111 L 00001
-//23456 e 67890
-//00001 o 00010
-//Sample Output 1:
-//
-//67890
-//Sample Input 2:
-//
-//00001 00002 4
-//00001 a 10001
-//10001 s -1
-//00002 a 10002
-//10002 t -1
-//Sample Output 2:
-//-1
 #include <iostream>
 #define max 100010
 using namespace std;
-
-
-
 int main() {
 	struct Node{
 		int data;
@@ -88,12 +57,5 @@ int main() {
 //		}
 //	if( k != -1) printf("%05d\n",k);
 	else printf("-1\n");
-
-
 	return 0;
 }
-//00001 00002 4
-//00001 a 10001
-//10001 s -1
-//00002 a 10002
-//10002 t -1
